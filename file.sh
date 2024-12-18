92cdcb06b18f83f904b5fd3a7b1b5032970f0a6d
adammosaad03 <mosaadadam647@gmail.com>
2024-12-17 19:26:47 -0500

    Create file.sh

---
+++ file.sh
@@ -0,0 +1,103 @@
+    1  echo hello terminal
+    2  psql --username=freecodecamp --dbname=postgres
+    3  touch bike-shop.sh
+    4  chmod +x bike-shop.sh
+    5  ./bike-shop.sh
+    6  ./bike-shop.sh
+    7  ./bike-shop.sh
+    8  ./bike-shop.sh
+    9  ./bike-shop.sh
+   10  ./bike-shop.sh
+   11  psql --username=freecodecamp --dbname=bikes
+   12  ./bike-shop.sh
+   13  ./bike-shop.sh
+   14  ./bike-shop.sh
+   15  ./bike-shop.sh
+   16  ./bike-shop.sh
+   17  ./bike-shop.sh
+   18  ./bike-shop.sh
+   19  ./bike-shop.sh
+   20  [[ a =~ [0-9] ]]; echo $?
+   21  [[ a1 =~ [0-9] ]]; echo $?
+   22  [[ a1 =~ ^[0-9]$ ]]; echo $?
+   23  [[ 1 =~ ^[0-9]$ ]]; echo $?
+   24  [[ 11 =~ ^[0-9]$ ]]; echo $?
+   25  [[ 11 =~ [0-9+$] ]]; echo $?
+   26  [ 11 =~ [0-9]+$ ]]; echo $
+   27  [[ 11 =~ [0-9]+$ ]]; echo $
+   28  [[ 11 =~ ^[0-9]+$ ]]; echo $?
+   29  [[ 11 !=~ ^[0-9]+$ ]]; echo $?
+   30  [[! 11 =~ ^[0-9]+$ ]]; echo $?
+   31  [[ ! 11 =~ ^[0-9]+$ ]]; echo $?
+   32  ./bike-shop.sh
+   33  ./bikeshop.sh
+   34  ./bike-shop.sh
+   35  ./bike-shop.sh
+   36  ./bike
+   37  ./bike-shop.sh
+   38  ./bike-shop.sh
+   39  ./bike-shop.sh
+   40  ./bike-shop.sh
+   41  echo '28 | Mountain' | sed 's/ /=/gi'
+   42   # get customer_id
+   43    # insert bike rental
+   44    # set bike availability to false
+   45    # get bike info
+   46    # send to main menu
+   47  echo '28 | Mountain' | sed 's/ /=/g'
+   48  echo '28 | Mountain' | sed 's/ //g'
+   49   echo '28 | Mountain' | sed 's/ //'
+   50   echo '28 | Mountain' | sed 's/ /'
+   51   echo '28 | Mountain' | sed 's/| //'
+   52  echo '28 | Mountain' | sed 's/ |//'
+   53  cho '28 | Mountain/"' | sed 's/ |//'
+   54  cho '28 | Mountain"' | sed 's/ |//'
+   55  cho '28 | Mountain\"' | sed 's/ |//'
+   56  echo '28 | Mountain"' | sed 's/ |//'
+   57  echo '28" | Mountain' | sed 's/ |//'
+   58  echo '28 | Mountain' | sed 's/ |/"/'
+   59  cho '28 | Mountain' | sed 's/ |//'
+   60  ./bike-shop.sh
+   61  ./bike-shop.sh
+   62  echo ' M e '
+   63  echo "$(echo ' M e ')."
+   64  echo "$(echo ' M e ' | sed 's/ //')."
+   65  echo "$(echo ' M e ' | sed 's/ s/s/')."
+   66  echo "$(echo ' M e ' | sed 's/g /g/g').
+   67  echo "$(echo ' M e ' | sed 's/ //g')."
+   68   echo "$(echo ' M e ' | sed 's/^ //g')."
+   69  echo "$(echo '    M e ' | sed 's/^ //g')."
+   70  echo "$(echo '    M e ' | sed 's/^ //g')."
+   71  echo "$(echo '   M e ' | sed 's/^ //g')."
+   72  echo "$(echo '   M e ' | sed 's/^ *//g')."
+   73  echo "$(echo '   M e ' | sed 's/^ $//g')."
+   74  echo "$(echo '   M e ' | sed 's/^ $//g')."
+   75  echo "$(echo '   M e ' | sed 's/ $//g')."
+   76  echo "$(echo '   M e   ' | sed 's/ $//g')."
+   77  echo "$(echo '   M e ' | sed 's/ $/g/g')."
+   78  echo "$(echo '   M e ' | sed 's/* $//g')."
+   79  echo "$(echo '   M e   ' | sed 's/* $//g')."
+   80  echo "$(echo '   M e ' | sed 's/ *$//g')."
+   81  echo "$(echo '   M e   ' | sed 's/ *$//g')."
+   82  echo "$(echo '   M e ' | sed 's/ *^|$//g')."
+   83  echo "$(echo '      M e    ' | sed 's/ *^|$//g')."
+   84  echo "$(echo '   M e ' | sed 's/^ *|  *$//g')."
+   85  echo "$(echo '       M e      ' | sed 's/^ *|  *$//g')."
+   86  echo "$(echo '   M e ' | sed 's/^  *|  *$//g')."
+   87  echo "$(echo '   M e   ' | sed 's/^ *| *$//g')."
+   88  man sed
+   89  ./bike-shop.sh
+   90  echo "$(echo '   M e   ' | sed -E 's/^ *| *$//g')."
+   91  ./bike-shop.sh
+   92  ./bike-shop.sh
+   93  ./bike-shop.sh
+   94  ./bike-shop.sh
+   95  ./bike-shop.sh
+   96  ./bike-shop.sh
+   97  ./bike-shop.sh
+   98  psql --username=freecodecamp --dbname=bikes
+   99  ./bike-shop.sh
+  100  ./bike-shop.sh
+  101  ./bike-shop/sh
+  102  ./bike-shop.sh
+  103  history > file.sh
